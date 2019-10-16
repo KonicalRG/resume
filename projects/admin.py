17f270595320871f914ef6d4b3b71622b4743be2
rgeronimo <rangergeronimo@gmail.com>
2019-10-16 10:57:04 -0400

    Portfolio Model added

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Portafolio
+
+
+class PortafolioAdminView(admin.ModelAdmin):
+    '''Admin View for Contact '''
+    list_display = ('title', 'category', 'author', )
+    list_filter = ('title', 'category', 'author', )
+    search_fields = ('title', 'category', 'author', )
+    ordering = ('title', 'category', 'author',)
+    editable_field = ('published')
+
+admin.site.register(Portafolio, PortafolioAdminView)
--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Portafolio(models.Model):
+    title = models.CharField(max_length=50, unique=True)
+    description = models.TextField()
+    author = models.CharField(max_length=50, blank=True)
+    category = models.CharField(max_length=50, editable=True)
+    wrapper_img = models.ImageField(upload_to='projects/wrapper')
+    modal_img = models.ImageField(upload_to=' projects/modal')
+    published = models.BooleanField(default=False)
