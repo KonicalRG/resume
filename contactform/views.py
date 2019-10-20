51a2f6f249148644730d1673258725c2106bf207
rgeronimo <rangergeronimo@gmail.com>
2019-10-20 15:31:59 -0400

    added readme.md and requirements files to repo

--- contactform/views.py
+++ contactform/views.py
@@ -5,8 +5,8 @@ from .forms import ContactForm
 
 class ContactFormView(CreateView, SuccessMessageMixin):
     form_class = ContactForm
-    # template_name = 'partials/_contact.html'
     template_name = 'index.html'
 
     def get_success_message(self, cleaned_data):
+        print(cleaned_data)
         return "your request was created successfully"
