89e0edefbda8a4a6e0d549169be39df909248390
rgeronimo <rangergeronimo@gmail.com>
2019-10-20 14:37:17 -0400

    Added message function

--- contactform/views.py
+++ contactform/views.py
@@ -1,7 +1,9 @@
+from django.contrib.messages.views import SuccessMessageMixin
 from django.views.generic.edit import CreateView
 from .forms import ContactForm
 
 
-class ContactFormView(CreateView):
+class ContactFormView(CreateView, SuccessMessageMixin):
     form_class = ContactForm
     template_name = 'partial/_contact.html'
+    success_message = "your request was created successfully"
