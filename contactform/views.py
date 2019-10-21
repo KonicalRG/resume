ceb238860896d8ab91760230d6e45dfdc94e7cde
rgeronimo <rangergeronimo@gmail.com>
2019-10-20 20:00:25 -0400

    Remove message feacture

--- contactform/views.py
+++ contactform/views.py
@@ -1,5 +1,6 @@
 from django.contrib.messages.views import SuccessMessageMixin
 from django.views.generic.edit import CreateView
+from resume.models import AcademicTraining, EmploymentHistory
 from .forms import ContactForm
 
 
@@ -10,3 +11,5 @@ class ContactFormView(CreateView, SuccessMessageMixin):
     def get_success_message(self, cleaned_data):
         print(cleaned_data)
         return "your request was created successfully"
+
+
