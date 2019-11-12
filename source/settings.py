8eaae174481f2cb05dda2e6d17b91edfc7d03bfb
rgeronimo <rangergeronimo@gmail.com>
2019-11-11 21:18:50 -0400

    Change db from Postgresql to sqlite for

--- source/settings.py
+++ source/settings.py
@@ -62,18 +62,18 @@ WSGI_APPLICATION = 'source.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'resume',
+    #     'USER': 'rgeronimo',
+    #     'PASSWORD': 'djangorocks',
+    #     'HOST': 'localhost',
+    #     'PORT': '',
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'resume',
-        'USER': 'rgeronimo',
-        'PASSWORD': 'djangorocks',
-        'HOST': 'localhost',
-        'PORT': '',
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
-
 
 
 # Password validation
